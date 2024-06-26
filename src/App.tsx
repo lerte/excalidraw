544de7e1603935cdbc870ffb32673c8da712e25e
lerte smith <lerte@qq.com>
2024-06-27 02:31:11 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,54 @@
-import { Excalidraw, WelcomeScreen } from "@excalidraw/excalidraw";
+import { Excalidraw, MainMenu, WelcomeScreen } from "@excalidraw/excalidraw";
+import { useEffect, useState } from "react";
 
-import TopRightUI from "./components/TopRightUI";
+import { ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types/types";
 
 const App = () => {
+  // @ts-ignore
+  const [excalidrawAPI, setExcalidrawAPI] = useState<ExcalidrawImperativeAPI>();
+
+  const disableContextMenuAfterBundle = () => {
+    if (!import.meta.env.DEV) {
+      document.oncontextmenu = (event) => {
+        event.preventDefault();
+      };
+    }
+  };
+
+  useEffect(() => {
+    disableContextMenuAfterBundle();
+    if (excalidrawAPI) {
+    }
+  }, [excalidrawAPI]);
+
   return (
     <div className="app h-full">
       <Excalidraw
+        excalidrawAPI={(api) => setExcalidrawAPI(api)}
         gridModeEnabled
         langCode="zh-CN"
-        renderTopRightUI={TopRightUI}
         initialData={{
+          scrollToContent: true,
           appState: {
             theme: "dark",
           },
         }}
       >
         <WelcomeScreen />
+        <MainMenu>
+          <MainMenu.Group>
+            <MainMenu.DefaultItems.LoadScene />
+            <MainMenu.DefaultItems.Export />
+            <MainMenu.DefaultItems.SaveAsImage />
+            <MainMenu.DefaultItems.Help />
+            <MainMenu.DefaultItems.ClearCanvas />
+          </MainMenu.Group>
+          <MainMenu.Separator />
+          <MainMenu.Group>
+            <MainMenu.DefaultItems.ToggleTheme />
+            <MainMenu.DefaultItems.ChangeCanvasBackground />
+          </MainMenu.Group>
+        </MainMenu>
       </Excalidraw>
     </div>
   );
