0b45fc080ad5a5d2d4dc8738a84e0fb894ac2ba7
lerte smith <lerte@qq.com>
2024-06-29 00:01:40 +0800

    feat:支持拖入.excalidraw和excalidrawlib文件

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,10 @@ import { useEffect, useState } from "react";
 import { AppMainMenu } from "./components/AppMainMenu";
 import CustomStats from "./CustomStats.tsx";
 import { NonDeletedExcalidrawElement } from "../packages/excalidraw/element/types";
+import { listen } from "@tauri-apps/api/event";
+import { readTextFile } from "@tauri-apps/plugin-fs";
 
 const App = () => {
-  // @ts-ignore
   const [excalidrawAPI, setExcalidrawAPI] = useState<ExcalidrawImperativeAPI>();
 
   const disableContextMenuAfterBundle = () => {
@@ -23,9 +24,7 @@ const App = () => {
 
   useEffect(() => {
     disableContextMenuAfterBundle();
-    if (excalidrawAPI) {
-    }
-  }, [excalidrawAPI]);
+  }, []);
 
   const renderCustomStats = (
     elements: readonly NonDeletedExcalidrawElement[],
@@ -39,6 +38,38 @@ const App = () => {
       />
     );
   };
+
+  type Payload = {
+    paths: string[];
+    position: {
+      x: number;
+      y: number;
+    };
+  };
+  useEffect(() => {
+    if (excalidrawAPI) {
+      listen<Payload>("tauri://drop", (event) => {
+        const { paths } = event.payload;
+        paths.map(async (path) => {
+          // 如果扩展名是.excalidraw (Excalidraw保存的文件格式)
+          if (path.endsWith(".excalidraw")) {
+            const content = await readTextFile(path);
+            const sceneData = JSON.parse(content);
+            for (let file in sceneData.files) {
+              excalidrawAPI.addFiles([sceneData.files[file]]);
+            }
+            excalidrawAPI.updateScene(sceneData);
+          }
+          // 如果扩展名是.excalidrawlib (Excalidraw保存的库文件格式)
+          if (path.endsWith("excalidrawlib")) {
+            const content = await readTextFile(path);
+            const library = JSON.parse(content);
+            excalidrawAPI.updateLibrary(library);
+          }
+        });
+      });
+    }
+  }, [excalidrawAPI]);
 
   return (
     <div className="app h-full">
