44c19cb772ca0730f180a74c4066b99ddf412d46
lerte smith <lerte@qq.com>
2024-07-10 21:51:54 +0800

    remove default export padding

--- packages/excalidraw/constants.ts
+++ packages/excalidraw/constants.ts
@@ -273,7 +273,7 @@ export const LIBRARY_SIDEBAR_WIDTH = parseInt(cssVariables.rightSidebarWidth);
 export const MAX_DECIMALS_FOR_SVG_EXPORT = 2;
 
 export const EXPORT_SCALES = [1, 2, 3];
-export const DEFAULT_EXPORT_PADDING = 10; // px
+export const DEFAULT_EXPORT_PADDING = 0; // px
 
 export const DEFAULT_MAX_IMAGE_WIDTH_OR_HEIGHT = 4096;
 
